f8bb5d7de1549c46f7db6b5c7d8cec2c68e0de79
Rishabh <asthana4@illinois.edu>
2017-12-28 11:05:48 -0600

    Testing toutes

--- server.js
+++ server.js
@@ -6,9 +6,16 @@ const app = express();
 app.post('/voice', (req, res) => {
 	const twiml = new VoiceResponse();
 	twiml.say('Hello, Wanli Wang is so hot!');
+	res.type('text/');
+	res.send(twiml.xmltoString());
+});
 
-	res.type('text/xml');
-	res.send(twiml.toString());
-})
+app.get('/' (req, res) => {
+	res.send('Server is up and running');
+});
 
-app.listen(8080);
+app.listen(80, function(){
+	console.log("Listening on port 80");
+});
+
+//https://demo.twilio.com/welcome/voice/
